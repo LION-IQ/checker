9f62c650ab9f8056037c5fdf32e8f406ca234fd5
matrix <64998320+shnider0@users.noreply.github.com>
2021-08-07 20:21:52 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,4 @@
-Run add-apt-repository ppa:deadsnakes/ppa
-Run add-apt-repository ppa:jonathonf/python-3.6
-Run apt-get update
-Run apt-get install python3.6
-Run apt install python3-pip
-Run python3.6 -m pip install -U setuptools
-Run python3.6 -m pip install -U pyrogram
-Run python3.6 -m pip install -U pyrogram[fast]
-Run python3.6 -m pip install redis
-Run apt-get install tor
-Run apt-get install torsocks
+
 python3 main.py
 echo """
              ╔═╦═╦══╦══╦══╦══╗╔╦╦══╦═╦═╦══╦═╦══╗
